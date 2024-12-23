19872ea8951616ac746be88399950643dccf3c92
Shubham Maske <shubhammaske099@gmail.com>
2024-12-23 17:29:06 +0530

    added the nested element

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement(
+    "div",
+    {id: 'parent'},
+    React.createElement(
+        "div", 
+        {id: "child"},
+        React.createElement("h1", {}, "I am an H1 tag")
+    )
+)
+console.log(parent)
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
