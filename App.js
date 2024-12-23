045ff1adabfe47d0a447b3055a450cb25c3dbe1a
Shubham Maske <shubhammaske099@gmail.com>
2024-12-23 18:04:26 +0530

    add multiple childs

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 const parent = React.createElement(
     "div",
-    {id: 'parent'},
-    React.createElement(
-        "div", 
-        {id: "child"},
-        [ React.createElement("h1", {}, "I am an H1 tag"),
-            React.createElement("h2", {}, "I am an H2 tag") 
-        ] //we can add multiple element using array
-    )
+    {id: 'parent'},[
+        React.createElement(
+            "div", 
+            {id: "child"},
+            [ React.createElement("h1", {}, "I am an H1 tag"),
+                React.createElement("h2", {}, "I am an H2 tag") 
+            ] //we can add multiple element using array
+        ),
+        React.createElement(
+            "div", 
+            {id: "child2"},
+            [ React.createElement("h1", {}, "I am an H1 tag from child 2"),
+                React.createElement("h2", {}, "I am an H2 tag child 2") 
+            ] //we can add multiple element using array
+        )
+    ]
+    
 )
 console.log(parent)
 
