62c2320fe8954b186c88534ec51aec9d55508817
Shubham Maske <shubhammaske099@gmail.com>
2024-12-26 14:40:01 +0530

    use of the jsx

--- App.js
+++ App.js
@@ -2,28 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 
-const parent = React.createElement(
-    "div",
-    {id: 'parent'},[
-        React.createElement(
-            "div", 
-            {id: "child"},
-            [ React.createElement("h1", {}, "I am an H1 tag"),
-                React.createElement("h2", {}, "I am an H2 tag") 
-            ] //we can add multiple element using array
-        ),
-        React.createElement(
-            "div", 
-            {id: "child2"},
-            [ React.createElement("h1", {}, "I am an H1 tag from child 2"),
-                React.createElement("h2", {}, "I am an H2 tag child 2") 
-            ] //we can add multiple element using array
-        )
-    ]
-    
+const jsxHeading = (
+    <h1 id= "heading"> Heading using JSX!</h1>
 )
-console.log(parent)
 
+console.log(jsxHeading)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(parent)
+root.render(jsxHeading)
