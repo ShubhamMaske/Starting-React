54b1a43228ac3c98c732a85f45a636e27902b4e9
Shubham Maske <shubhammaske099@gmail.com>
2025-01-04 16:21:39 +0530

    use react hook- useState() to track the changes in the functional component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,23 @@
 import RestaurantCard from "./RestaurantCard"
 import restaurantList from "../utils/mockData"
+import { useState } from "react"
 
 
 const Body = () => {
+
+    const [listOfRestaurants, setlistOfRestaurants] = useState(restaurantList);
+
     return (
         <div className="body">
             <div className="Search">search</div>
+            <button className="filter-btn" onClick={ () => {
+                const filteredList = listOfRestaurants.filter(
+                    (res) => res.info.avgRating > 4.4
+                );
+                setlistOfRestaurants(filteredList)
+            }}>
+                Top Rated Restaurants
+            </button>
             <div className="res-container">
                 {/* <RestaurantCard resData={restaurantList[0]}/>
                 <RestaurantCard resData={restaurantList[1]}/>
@@ -15,7 +27,7 @@ const Body = () => {
                 */}
                 {
                   
-                  restaurantList.map((restaurant) => {
+                  listOfRestaurants.map((restaurant) => {
                     return <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
                   })
                 }
