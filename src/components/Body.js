621c72ed4b92ab1838683ad0a1e2ee8b6257b132
Shubham Maske <shubhammaske099@gmail.com>
2025-01-16 12:19:18 +0530

    used api for live data and render on screen with help of useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,39 +1,60 @@
-import RestaurantCard from "./RestaurantCard"
-import restaurantList from "../utils/mockData"
-import { useState } from "react"
-
+import RestaurantCard from './RestaurantCard'
+import restaurantList from '../utils/mockData'
+import { useState, useEffect } from 'react'
+import Shimmer from './Shimmer'
+import { SWIGGY_URL } from '../utils/constants'
 
 const Body = () => {
+  const [listOfRestaurants, setlistOfRestaurants] = useState([])
 
-    const [listOfRestaurants, setlistOfRestaurants] = useState(restaurantList);
+  // it use after the component rendered
+  useEffect(() => {
+    fetchData()
+  }, []);
 
-    return (
-        <div className="body">
-            <div className="Search">search</div>
-            <button className="filter-btn" onClick={ () => {
-                const filteredList = listOfRestaurants.filter(
-                    (res) => res.info.avgRating > 4.4
-                );
-                setlistOfRestaurants(filteredList)
-            }}>
-                Top Rated Restaurants
-            </button>
-            <div className="res-container">
-                {/* <RestaurantCard resData={restaurantList[0]}/>
+  const fetchData = async () => {
+    const data = await fetch(SWIGGY_URL);
+
+    const json = await data.json();
+
+    //optional chaining
+    // console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setlistOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
+
+  if(listOfRestaurants.length === 0) {
+    return <Shimmer />
+  }
+
+  return (
+    <div className='body'>
+      <div className='Search'>search</div>
+      <button
+        className='filter-btn'
+        onClick={() => {
+          const filteredList = listOfRestaurants.filter(
+            res => res.info.avgRating > 4.4
+          )
+          setlistOfRestaurants(filteredList)
+        }}
+      >
+        Top Rated Restaurants
+      </button>
+      <div className='res-container'>
+        {/* <RestaurantCard resData={restaurantList[0]}/>
                 <RestaurantCard resData={restaurantList[1]}/>
                 <RestaurantCard resData={restaurantList[2]}/>
                 <RestaurantCard resData={restaurantList[3]}/> 
                 Instead of this we use js map to loop over resList array and write inside {} braces
                 */}
-                {
-                  
-                  listOfRestaurants.map((restaurant) => {
-                    return <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
-                  })
-                }
-            </div>
-        </div>    
-    )
+        {listOfRestaurants.map(restaurant => {
+          return (
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
-export default Body;
+export default Body
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,9 @@
-
-
 export const LOGO_URL = 
     "https://png.pngtree.com/png-vector/20221231/ourmid/pngtree-food-delivery-logo-template-design-sign-menu-vector-png-image_43664212.jpg"
 
 
 export const CDN_URL = 
     "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+
+export const SWIGGY_URL = 
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2097179&lng=72.9496616&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
