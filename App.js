8cf2613ad9a31d969b670469e0d5c7e660379235
Shubham Maske <shubhammaske099@gmail.com>
2025-01-18 13:16:22 +0530

    added react router logic using react-router for contact,about and error

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import Body from "./src/components/Body"
 import Header from "./src/components/Header"
-
+import About from "./src/components/About"
+import Contact from "./src/components/Contact"
+import Error from "./src/components/Error"
+import { createBrowserRouter, RouterProvider } from "react-router"
 
 const AppLayout = () => {
     return (
@@ -13,5 +16,22 @@ const AppLayout = () => {
     )
 }
 
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        errorElement: <Error />,
+    },
+    {
+        path: "/about",
+        element: <About />
+    },
+    {
+        path: "/contact",
+        element: <Contact />
+    }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<AppLayout />)
+
+root.render(<RouterProvider router={appRouter} />)
